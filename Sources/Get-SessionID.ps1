4d1177fc2b3ed4f1662438fbf4a85557fc6c6c12
Oak Smith <Oak@9inch.com>
2024-06-04 15:07:25 +0700

    Test

--- Sources/Get-SessionID.ps1
+++ Sources/Get-SessionID.ps1
@@ -1,7 +1,7 @@
 Try {
 	# Check if we are running in Session 0
-	If (([System.Diagnostics.Process]::GetCurrentProcess() | Select "SessionID" -ExpandProperty "SessionID") -eq 0) { 
-		Exit 1 # Running in Session 0 
+	If (([System.Diagnostics.Process]::GetCurrentProcess() | Select "SessionID" -ExpandProperty "SessionID") -eq 0) {
+		Exit 1 # Running in Session 0
 	}
 	Else {
 		Exit 0 # Not running in Session 0
