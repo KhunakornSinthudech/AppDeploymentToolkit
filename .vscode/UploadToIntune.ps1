a16319c06cc5530ba00fae8126294f03d579c6aa
Oak Smith <Oak@9inch.com>
2024-06-04 14:59:06 +0700

    TEST

--- .vscode/UploadToIntune.ps1
+++ .vscode/UploadToIntune.ps1
@@ -1,17 +1,18 @@
 # Vars
 . ".vscode\Global.ps1"
 
-#Install-Module -Name "IntuneWin32App" -force
+Install-Module -Name "IntuneWin32App" -force
 Get-installedModule -Name IntuneWin32App
 # Retrieve auth token required for accessing Microsoft Graph
 # Delegated authentication is currently supported only, app-based authentication is on the todo-list
 Connect-MSIntuneGraph -TenantID '2ca58655-3ad3-4836-b3fa-fd15586561a5' -Verbose
 
 
-    $Publisher = "Khunakorn SInthudech"
+
     $IntuneWinFile = "$Desktop\$Application.intunewin"
     $AppIconFile = "Toolkit\Icon\$Application.png"
-
+    $IntuneWinMetaData = Get-IntuneWin32AppMetaData -FilePath $IntuneWinFile
+    $Publisher = $IntuneWinMetaData.ApplicationInfo.msiinfo.MsiPublisher
     # Create custom display name like 'Name' and 'Version'
     $DisplayName = "$Application Demo Khunakorn"
     Write-Output -InputObject "Constructed display name for Win32 app: $($DisplayName)"
@@ -24,7 +25,7 @@ Connect-MSIntuneGraph -TenantID '2ca58655-3ad3-4836-b3fa-fd15586561a5' -Verbose
     # $DetectionScriptFile = "Toolkit\Detection\Detection.ps1"
     # $DetectionRule = New-IntuneWin32AppDetectionRuleScript -ScriptFile $DetectionScriptFile -EnforceSignatureCheck $false -RunAs32Bit $false
     # Create MSI detection rule
-    $IntuneWinMetaData = Get-IntuneWin32AppMetaData -FilePath $IntuneWinFile
+
     $DetectionRule = New-IntuneWin32AppDetectionRuleMSI -ProductCode $IntuneWinMetaData.ApplicationInfo.MsiInfo.MsiProductCode
 
     # Convert image file to icon
@@ -33,9 +34,11 @@ Connect-MSIntuneGraph -TenantID '2ca58655-3ad3-4836-b3fa-fd15586561a5' -Verbose
     # Add new EXE Win32 app
     $InstallCommandLine = "Deploy-Application.exe"
     $UninstallCommandLine = "Deploy-Application.exe Uninstall"
-    Add-IntuneWin32App -FilePath $IntuneWinFile -DisplayName $DisplayName -Description "7Zip is the best file zipper ever!" -Publisher $Publisher -InstallExperience "system" -RestartBehavior "suppress" -DetectionRule $DetectionRule -RequirementRule $RequirementRule -InstallCommandLine $InstallCommandLine -UninstallCommandLine $UninstallCommandLine -Icon $Icon -Verbose
+    $AppReference = Add-IntuneWin32App -FilePath $IntuneWinFile -DisplayName $DisplayName -Description "7Zip is the best file zipper ever!" -AppVersion $IntuneWinMetaData.ApplicationInfo.msiinfo.MsiProductVersion   -Publisher $Publisher -InstallExperience "system" -RestartBehavior "suppress" -DetectionRule $DetectionRule -RequirementRule $RequirementRule -InstallCommandLine $InstallCommandLine -UninstallCommandLine $UninstallCommandLine -Icon $Icon -Verbose
 
     #Write-Output -InputObject "Starting to create Win32 app in Intune"
     #$Win32App = Add-IntuneWin32App @Win32AppArguments
 
     Write-Output -InputObject "Successfully created new Win32 app with name: $($DisplayName)"
+    Add-IntuneWin32AppAssignmentAllUsers -ID $AppReference.id -Intent "available" -Notification "showAll" -Verbose
+    Write-Output -InputObject "$($DisplayName) Available to all users"
